6b23e7c63b6bf490a2602ea8b8a2d9388aba322a
Emmanuel Chijioke <manuelckay@gmail.com>
2024-03-14 04:37:54 +0100

    About me page refixed

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,27 +16,27 @@ const AboutMe = () => {
           />
         </div>
         <div className="text-gray-600 w-full">
-          <h1 className="text-3xl text-center md:text-left font-semibold text-gray-900 mb-6">About Chioma Ezenwafor</h1>
+          <h1 className="text-3xl text-center md:text-left font-semibold text-gray-900 mb-6">About Me</h1>
           <p className="mb-4">
-            Chioma Ezenwafor is a journalist with a decade-long experience in the media industry, and a Media and Communications Consultant. The News Manager of the foremost audio-visual news and talk radio stations, 92.3 Nigeria Info FM and Music stations, 95.9 Cool FM and 94.1 Wazobia FM in Port Harcourt. She is an alumna of the 2022 The Edward R. Murrow Program for Journalists of the International Visitor Leadership Program (IVLP) of the U.S Department of State.
+            I am a journalist with a decade-long experience in the media industry, and a Media and Communications Consultant. The News Manager of the foremost audio-visual news and talk radio stations, 92.3 Nigeria Info FM and Music stations, 95.9 Cool FM and 94.1 Wazobia FM in Port Harcourt. I am an alumna of the 2022 The Edward R. Murrow Program for Journalists of the International Visitor Leadership Program (IVLP) of the U.S Department of State.
           </p>
           <p className="mb-4">
             A member of the Society of Nigerian Broadcasters (SNB), Nigerian Union of Journalists (NUJ), African Council for Communication Education (ACCE), International Communication Association (ICA) and the Journalism Educators Foundation (JEF). The host of the business show, Talking Figures on 92.3 Nigeria Info FM.
           </p>
           <p className="mb-4">
-            She is the author of "Becoming A News Titan" and co-authored "Dream Nation by Design." She is the convener of "Ink and Insights," a book signing hybrid event that holds every quarter. Her articles have been published on nigeriainfo.fm, newstimedaily.ng, reportingmigration.org and isparkng.com. Her work on radio news spans over a decade and has impacted so many lives.
+            I am the author of &quot;Becoming A News Titan&quot; and co-authored &quot;Dream Nation by Design.&quot; I am the convener of &quot;Ink and Insights,&quot; a book signing hybrid event that holds every quarter. My articles have been published on <Link href="https://www.nigeriainfo.fm/port-harcourt/" className="link link-hover text-red-500" target="_blank" rel="noopener noreferrer">nigeriainfo.fm</Link>. My work on radio news spans over a decade and has impacted so many lives.
           </p>
           <p className="mb-4">
-            She is the creator of the Reporting Political and Civic Participation Course for the African Women in Media (AWiM) organisation. She also created a Content Writing and Content Creation Course. Her contributions have led to the success of events and Media Projects like the Startup Port Harcourt Week and a reality TV show for Entrepreneurs, the Grind TV Show.
+            I am the creator of the Reporting Political and Civic Participation Course for the African Women in Media (AWiM) organisation. I also created a Content Writing and Content Creation Course. My contributions have led to the success of events and Media Projects like the Startup Port Harcourt Week and a reality TV show for Entrepreneurs, the Grind TV Show.
           </p>
           <p className="mb-4">
-            She won The Media Awards prize for News Editor of the Year 2019 edition and was listed among 50 African Women in Media celebrated for working actively in media and journalism across the continent of Africa and leading change, fostering development across sectors and impacting their local communities.
+            I won The Media Awards prize for News Editor of the Year 2019 edition and was listed among 50 African Women in Media celebrated for working actively in media and journalism across the continent of Africa and leading change, fostering development across sectors and impacting their local communities.
           </p>
           <p className="mb-4">
-            She was announced the Prize Winner of the Reporting Migration Competition in 2020 launched by the UN Migration, that's the International Organization for Migration, IOM. She is a TEDx speaker and UN75 Speaker. She has several training certificates in various areas like Writing for the Web, social media and Journalism, Gender Reporting, Conflict Sensitive Reporting, Family Planning Reporting, Common Ground Journalism, Peace Journalism, Investigative Journalism, Solution Journalism to mention a few.
+            I was announced the Prize Winner of the Reporting Migration Competition in 2020 launched by the UN Migration, that&apos;s the International Organization for Migration, IOM. I am a TEDx speaker and UN75 Speaker. She has several training certificates in various areas like Writing for the Web, social media and Journalism, Gender Reporting, Conflict Sensitive Reporting, Family Planning Reporting, Common Ground Journalism, Peace Journalism, Investigative Journalism, Solution Journalism to mention a few.
           </p>
           <p className="mb-4">
-            She has a Bachelor's Degree in Mass communication from the Nnamdi Azikiwe University, Awka Anambra state, and a Masters degree in Communication from the University of Port Harcourt. She is currently pursuing a Ph.D. in Journalism and Media Studies. She is married to Engineer Chukwudi Ezenwafor and they both live in Port Harcourt, Rivers State, Nigeria with their three amazing kids, Zina, Sineto, and Aiyima.
+            I have a Bachelor&apos;s Degree in Mass communication from the Nnamdi Azikiwe University, Awka Anambra state, and a Masters degree in Communication from the University of Port Harcourt. I am currently pursuing a Ph.D. in Journalism and Media Studies. I am married to Engineer Chukwudi Ezenwafor and live in Port Harcourt, Rivers State, Nigeria with our three amazing kids, Zina, Sineto, and Aiyima.
           </p>
           <div className="flex justify-center md:justify-start mt-4">
             {/* Social Media Links */}
@@ -51,7 +51,7 @@ const AboutMe = () => {
         </div>
       </div>
     </div>
-  );
-};
-
-export default AboutMe;
+    );
+  };
+  
+  export default AboutMe;
